2090a90b8096a6ade44785a73a32fc3888424203
peterndungukamau <peterndungukamau9@gmail.com>
2019-10-26 13:58:55 +0300

    add quote details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
 
-  quotes:string[];
-
-  constructor(){
-    this.quotes = ['"If you judge people, you have no time to love them. Mother Teresa "','"The greatest wealth is to live content with little. Plato "']
-  }
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote = [
+    {author:'Mother Teresa', quote:'If you judge people, you have no time to love them.', submitter:'Peter', more:'another quote from Teresa'},
+    {author:'Plato', quote:'The greatest wealth is to live content with little.', submitter:'Paswell', more:'another quote from Plato'}
+  ];
 
   constructor() { }
 
